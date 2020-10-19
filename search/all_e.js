454f9c69106b5618717b0a85bbd13a08257176f1
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2020-10-19 00:17:38 +0000

    deploy: d2ea155cc7989d54bbdda4c7a60d6caa8e7e0a9d

--- search/all_e.js
+++ search/all_e.js
@@ -3,7 +3,7 @@ var searchData=
   ['parent_110',['parent',['../structfcarouge_1_1tree_1_1internal__node__type.html#abb38b1b9d4f98d6e22d6e88797e6c4d2',1,'fcarouge::tree::internal_node_type']]],
   ['pointer_111',['pointer',['../classfcarouge_1_1tree.html#aa318e3abfbfa6655010512a9727b7ac2',1,'fcarouge::tree::pointer()'],['../structfcarouge_1_1tree_1_1internal__iterator__type.html#a87df2c96096d64e9c9cab3bfcfe94f4e',1,'fcarouge::tree::internal_iterator_type::pointer()'],['../structfcarouge_1_1tree_1_1internal__const__iterator__type.html#ad4e88162f419db225b877b7f731f936c',1,'fcarouge::tree::internal_const_iterator_type::pointer()']]],
   ['position_112',['position',['../structfcarouge_1_1tree_1_1insert__return__type.html#adf0b0aabf76dc5f86e2bfce55f15a997',1,'fcarouge::tree::insert_return_type']]],
-  ['prune_113',['prune',['../classfcarouge_1_1tree.html#a8aef92f2828d78713bca539062ce9fae',1,'fcarouge::tree']]],
+  ['prune_113',['prune',['../classfcarouge_1_1tree.html#a18e389dee96b6100d05cc0faee224f91',1,'fcarouge::tree']]],
   ['publish_114',['publish',['../_l_i_c_e_n_s_e_8txt.html#a9874a8ace1386980f514ee0ac38c8a0c',1,'LICENSE.txt']]],
   ['push_115',['push',['../classfcarouge_1_1tree.html#ace557ab09e595974d1250f1c8f8d0388',1,'fcarouge::tree::push(const_iterator position, const_reference value)'],['../classfcarouge_1_1tree.html#af36c3388ad91a67e280d2ea91eb61513',1,'fcarouge::tree::push(const_iterator position, value_type &amp;&amp;value)']]],
   ['push_5ffront_116',['push_front',['../classfcarouge_1_1tree.html#a7182fde5a67b4db4c3560cc59249b358',1,'fcarouge::tree::push_front(const_reference value)'],['../classfcarouge_1_1tree.html#ad78c82126d1345ff4d85e10ec76f674e',1,'fcarouge::tree::push_front(value_type &amp;&amp;value)']]],
